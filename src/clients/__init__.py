e76652c423a588803fbbb21d9dd6b5a4db2ef00b
Wladymir Brborich <wabrborich@hotmail.com>
2022-04-19 15:01:34 -0500

    Add a basic requests based rest client
    * This client supports various auth methods to interact with external APIs
    
    * It wraps GET and POST requests
    
    * It could definitely be improved

---
+++ src/clients/__init__.py
@@ -0,0 +1,2 @@
+# flake8: noqa
+from .rest import RestClient
---
+++ src/clients/rest.py
@@ -0,0 +1,17 @@
+
+import requests
+from typing import Optional
+
+
+class RestClient:
+
+    def __init__(self, base_url: str, api_token: Optional[str] = None, is_bearer: bool = False, is_basic_auth: bool = False) -> 'RestClient':
+        self.base_url = base_url.rstrip('/')
+        self.headers = {'Authorization': f'Bearer {api_token}'} if api_token and is_bearer else {}
+        self.basic_auth = (api_token, '') if api_token and is_basic_auth else None
+
+    def get(self, additional_url: str = '', query_params: Optional[dict] = None) -> requests.Response:
+        return requests.get(f'{self.base_url}/{additional_url}', params=query_params or {}, headers=self.headers, auth=self.basic_auth)
+
+    def post(self, additional_url: str = '', payload: Optional[dict] = None) -> requests.Response:
+        return requests.post(f'{self.base_url}/{additional_url}', json=payload, headers=self.headers, auth=self.basic_auth)
