6552e505d3b4c6b861a19a50d81f572fecf9aef0
Wladymir Brborich <wabrborich@hotmail.com>
2022-04-19 15:03:52 -0500

    Add some birthday wishes templates
    These are some messages I took from birthday bot, I guess we can include ours at some point

---
+++ src/data/wishes.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+
+BIRTHDAY_WISH_TEMPLATES = (
+    '{}\nThe whole office voted, and it’s my duty to tell you …\nHAPPY BIRTHDAY! Pull crackers and party hats!',
+    'Hey {},\nI want to take this opportunity to wish you a happy, happy birthday. You deserve it!\nThank you for everything you do around the office. You are a special friend and colleague here.',
+    'Hi {},\nAfter careful consideration, it’s time you moved UP! … In years, that is!\nHappy Birthday! It’s time to cut loose!',
+    'Today just got better because {} has a birthday!\nHappy Birthday! We wish you all the best!',
+    'Dear \n,Happy birthday! Happy birthday! Happy birthday! Throw on your favorite music and hop into a nice, warm bath. You’ve earned this year, and you’re going to need your rest for next year.\nI have a feeling it’s going to be a doozy!',
+    'Hi {},\nHate to do this via message, but we’re gonna have to … WISH YOU A HAPPY BIRTHDAY and party it up!\nYou ROCK! Have a great day!'
+)
