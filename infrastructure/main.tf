77fa5ee8c55ccddba4ecb707de5aed4c7f118d84
Jefferson Oña <67077796+jeffqev@users.noreply.github.com>
2022-04-29 16:41:27 -0500

    Terraform implementation (#3)
    * terraform implementation
    
    * suggestions

---
+++ infrastructure/main.tf
@@ -0,0 +1,79 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.9.0"
+    }
+  }
+  backend "s3" {
+    bucket = "tfstate-bucket-ioet-birthday-bot"
+    key    = "terraform/state/birthday_lambda.tfstate"
+    region = "us-east-1"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region     = var.REGION
+  access_key = var.ACCESS_KEY
+  secret_key = var.SECRET_KEY
+}
+
+
+data "aws_secretsmanager_secret" "secret" {
+  name = var.SECRET_NAME
+}
+
+data "aws_secretsmanager_secret_version" "creds" {
+  secret_id = data.aws_secretsmanager_secret.secret.id
+}
+
+locals {
+  secret = jsondecode(
+    data.aws_secretsmanager_secret_version.creds.secret_string
+  )
+}
+
+
+module "lambda_function_container_image" {
+  source = "terraform-aws-modules/lambda/aws"
+
+  function_name = "ioet-birthday-bot"
+  description   = "Birthday-bot sends wishes each day at 9:00 AM"
+
+  create_package = false
+
+  environment_variables = {
+    BAMBOOHR_API_TOKEN        = local.secret.BAMBOOHR_API_TOKEN
+    BAMBOOHR_SUBDOMAIN        = local.secret.BAMBOOHR_SUBDOMAIN
+    SLACK_WEBHOOK_URL_SECRET  = local.secret.SLACK_WEBHOOK_URL_SECRET
+    SLACK_BOT_USER_AUTH_TOKEN = local.secret.SLACK_BOT_USER_AUTH_TOKEN
+    TENOR_API_KEY             = local.secret.TENOR_API_KEY
+    UTC_HOUR_OFFSET           = local.secret.UTC_HOUR_OFFSET
+  }
+
+  image_uri    = var.DOCKER_IMAGE
+  package_type = "Image"
+}
+
+
+resource "aws_cloudwatch_event_rule" "schedule" {
+  name                = "every-day"
+  description         = "Trigger every day at 9:00 AM"
+  schedule_expression = "cron(0 9 * * ? *)"
+}
+
+
+resource "aws_cloudwatch_event_target" "check_schedule" {
+  rule      = aws_cloudwatch_event_rule.schedule.name
+  target_id = "schedule_lambda"
+  arn       = module.lambda_function_container_image.lambda_function_arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = module.lambda_function_container_image.lambda_function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.schedule.arn
+}
