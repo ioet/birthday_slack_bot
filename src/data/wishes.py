0fc0791fcaa88b74484f99b07dcf138fa2264e85
jcalarcon98 <jeancalarcon98@gmail.com>
2022-05-07 13:51:02 -0500

    Add more birthday templates

--- src/data/wishes.py
+++ src/data/wishes.py
@@ -1,10 +1,17 @@
 # flake8: noqa
 
 BIRTHDAY_WISH_TEMPLATES = (
-    '{}\nThe whole office voted, and it’s my duty to tell you …\nHAPPY BIRTHDAY! Pull crackers and party hats!',
-    'Hey {},\nI want to take this opportunity to wish you a happy, happy birthday. You deserve it!\nThank you for everything you do around the office. You are a special friend and colleague here.',
+    '{}\nThe whole office voted, and it’s my duty to tell you …\nHAPPY BIRTHDAY! Pull crackers and party hats! :pepe-jedi-party:',
+    'Hey {},\nI want to take this opportunity to wish you a happy, happy birthday. You deserve it!\nThank you for everything you do around the office. You are a special friend and colleague here. :dance-party:',
     'Hi {},\nAfter careful consideration, it’s time you moved UP! … In years, that is!\nHappy Birthday! It’s time to cut loose!',
-    'Today just got better because {} has a birthday!\nHappy Birthday! We wish you all the best!',
-    'Dear {},\nHappy birthday! Happy birthday! Happy birthday! Throw on your favorite music and hop into a nice, warm bath. You’ve earned this year, and you’re going to need your rest for next year.\nI have a feeling it’s going to be a doozy!',
-    'Hi {},\nHate to do this via message, but we’re gonna have to … WISH YOU A HAPPY BIRTHDAY and party it up!\nYou ROCK! Have a great day!'
+    'Today just got better because {} has a birthday!\nHappy Birthday :pepe-jedi-party:! We wish you all the best :pepe-party:!',
+    'Dear {},\nHappy birthday! Happy birthday! Happy birthday! Throw on your favorite music and hop into a nice, warm bath. You’ve earned this year, and you’re going to need your rest for next year.\nI have a feeling it’s going to be a doozy! :rainbow-party:',
+    'Hi {},\nHate to do this via message, but we’re gonna have to … WISH YOU A HAPPY BIRTHDAY and party it up!\nYou ROCK! Have a great day! :blob-cat-party: :dog-party:',
+    'Hey {}, it’s time for an ioet birthday party :ioet: :partying_face:\nYou know what that means:\ncake :birthday:\ndrinks :beers:\nkaraoke :microphone: :marievelyn:\nand more :confetti_ball:\nEnjoy your day and make sure your coworkers celebrate you as they should',
+    'It’s {}’s birthday! Thanks for sharing your special day with us here at :ioet::balloon::smiling_face_with_3_hearts:\nMaybe in the future they’ll let bots like me join the party :thinking_face::dancer:',
+    'It may be a good option to start with this icon :pug-dance-party: instead of this one :partying_face:, because it’s your big party {} lol! :rainbow-party: :blob-cat-party:',
+    'Dear {}\nIt’s your birthday\nI look forward to hearing about your party :rainbow-party:',
+    'Today is {} birthday :partying_face:, that means it’s time for SALSA, and I mean dance, not the sauce :dancer::man_dancing::tada:',
+    'Feliz cumpleaños {}, este año si te toca aprender salsa para hacer feliz a :marievelyn: :rolling_on_the_floor_laughing:!',
+    'Feliz cumpleaños {}, en lugar de correazos :ioet: tiene el derecho a hacerte bullying todo el día. Salu2! :blob-party:',
 )
