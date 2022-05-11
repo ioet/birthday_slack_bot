05f85300d67af9960e6bbe6da3e0c3b3b8c95a7c
jcalarcon98 <jeancalarcon98@gmail.com>
2022-05-11 17:07:28 -0500

    Add Anniversary Controller

--- src/utils/dateutils.py
+++ src/utils/dateutils.py
@@ -8,3 +8,9 @@ def get_day_month(date: datetime) -> tuple:
 def get_current_day_month(utc_hour_offset: str) -> tuple:
     current_date = datetime.utcnow() + timedelta(hours=int(utc_hour_offset))
     return get_day_month(current_date)
+
+
+def get_years_difference_from_current_date(utc_hour_offset: str, date: str, date_format: str = '%Y-%m-%d') -> int:
+    date = datetime.strptime(date, date_format)
+    current_date = datetime.utcnow() + timedelta(hours=int(utc_hour_offset))
+    return current_date.year - date.year
