754fa52ad230e4c8f4a88b8f6ef3a442989de653
Wladymir Brborich <wabrborich@hotmail.com>
2022-06-15 12:06:26 -0500

    Add custom headers to get and post requests
    * This allows us to signal the bamboo api the data return type (json or xml)

--- src/clients/rest.py
+++ src/clients/rest.py
@@ -10,8 +10,10 @@ class RestClient:
         self.headers = {'Authorization': f'Bearer {api_token}'} if api_token and is_bearer else {}
         self.basic_auth = (api_token, '') if api_token and is_basic_auth else None
 
-    def get(self, additional_url: str = '', query_params: Optional[dict] = None) -> requests.Response:
-        return requests.get(f'{self.base_url}/{additional_url}', params=query_params or {}, headers=self.headers, auth=self.basic_auth)
+    def get(self, additional_url: str = '', query_params: Optional[dict] = None, custom_headers: Optional[dict] = None) -> requests.Response:
+        headers_to_send = {**self.headers, **custom_headers} if custom_headers else self.headers
+        return requests.get(f'{self.base_url}/{additional_url}', params=query_params or {}, headers=headers_to_send, auth=self.basic_auth)
 
-    def post(self, additional_url: str = '',  payload: Optional[dict] = None, query_params: Optional[dict] = None,) -> requests.Response:
-        return requests.post(f'{self.base_url}/{additional_url}', params=query_params or {}, json=payload, headers=self.headers, auth=self.basic_auth)
+    def post(self, additional_url: str = '',  payload: Optional[dict] = None, query_params: Optional[dict] = None, custom_headers: Optional[dict] = None) -> requests.Response:
+        headers_to_send = {**self.headers, **custom_headers} if custom_headers else self.headers
+        return requests.post(f'{self.base_url}/{additional_url}', params=query_params or {}, json=payload, headers=headers_to_send, auth=self.basic_auth)
