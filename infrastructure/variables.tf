1a2c0505bbcdd3224d253d1079e80a403ab81fff
Jefferson Oña <67077796+jeffqev@users.noreply.github.com>
2022-06-21 19:05:37 -0500

    Fix repeated lambda calls when an error occurs (#11)
    * dont call again when lambda returns an error
    
    * use sensitive variables in terraform vars

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,6 +1,24 @@
-variable "REGION" {}
-variable "ACCESS_KEY" {}
-variable "SECRET_KEY" {}
-
-variable "DOCKER_IMAGE" {}
-variable "SECRET_NAME" {}
+variable "REGION" {
+  description = "Aws region"
+  type        = string
+}
+variable "ACCESS_KEY" {
+  description = "Aws access key"
+  type        = string
+  sensitive   = true
+}
+variable "SECRET_KEY" {
+  description = "Aws secret key"
+  type        = string
+  sensitive   = true
+}
+variable "DOCKER_IMAGE" {
+  description = "Docker image name for the lambda that is stored in ECR"
+  type        = string
+  sensitive   = true
+}
+variable "SECRET_NAME" {
+  description = "Name of the secret to be consulted from aws secret manager"
+  type        = string
+  sensitive   = true
+}
