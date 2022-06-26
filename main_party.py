909510133b8268f647dd95f20df29c98502b6ab7
Wladymir Brborich <wabrborich@hotmail.com>
2022-06-26 14:06:04 -0500

    Sort imports in main.py

--- main_party.py
+++ main_party.py
@@ -1,13 +1,13 @@
 import http
 import logging
 
+from src.controllers.anniversary_message import AnniversaryMessageController
+from src.controllers.birthday_message import BirthdayMessageController
+from src.data.wishes import ANNIVERSARY_WISH_TEMPLATES, BIRTHDAY_WISH_TEMPLATES
 from src.integrations.bamboo import BambooIntegration
 from src.integrations.slack_api import SlackApiIntegration
 from src.integrations.slack_message import SlackMessageIntegration
 from src.integrations.tenor_gif import TenorGifIntegration
-from src.controllers.birthday_message import BirthdayMessageController
-from src.controllers.anniversary_message import AnniversaryMessageController
-from src.data.wishes import BIRTHDAY_WISH_TEMPLATES, ANNIVERSARY_WISH_TEMPLATES
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
