0db3d17fab1b7e55ff7e2058a4d63f45c2aa753e
Wladymir Brborich <wabrborich@hotmail.com>
2022-07-19 15:18:24 -0500

    Improve performance using concurrent requests
    * Use asyncio.gather to run all message requests concurrently
    
    * This improves our performance when there are a bunch of messages to send, preventing a timeout

--- src/controllers/birthday_message.py
+++ src/controllers/birthday_message.py
@@ -1,3 +1,4 @@
+import asyncio
 from typing import List, Tuple
 
 from src.controllers.base import BaseController
@@ -10,13 +11,20 @@ class BirthdayMessageController(BaseController):
 
     @staticmethod
     def get_birthday_employees(employees: List[dict], birthday_field) -> List[dict]:
-        return [employee for employee in employees if date_utils.is_current_date(employee.get(birthday_field), '%m-%d')]
+        return (employee for employee in employees if date_utils.is_current_date(employee.get(birthday_field), '%m-%d'))
 
     @classmethod
-    def send(cls, hr_integration, slack_api_integration, slack_message_integration, gif_integration, templates: Tuple[str]):
-        birthday_employees = cls.get_birthday_employees(hr_integration.get_employees_with_birthday(), hr_integration.employee_birthday_field)
-        birthday_employees_ids = slack_api_integration.get_members_id_by_email(hr_integration.get_employees_email(birthday_employees))
+    async def send(cls, employee_manager, slack_api_integration, slack_message_integration, gif_integration, templates: Tuple[str]):
+
+        async def send_message_coro(best_matching_keyword: str, gif_search_limit: int, message: str):
+            selected_gif = await gif_integration.get_random_gif(best_matching_keyword, gif_search_limit)
+            await slack_message_integration.send_message(message, selected_gif.get('url'), selected_gif.get('description'))
+
+        birthday_employees = cls.get_birthday_employees(await employee_manager.get_employees_with_birthday(),
+                                                        employee_manager.integration.employee_birthday_field)
+        birthday_employees_ids = await slack_api_integration.get_members_id_by_email(employee_manager.integration.get_employees_email(birthday_employees))
         templates_copy = list(templates)
+        message_coros = []
         for employee_id in birthday_employees_ids:
             template = cls.choose_template(templates_copy)
             if len(templates_copy) > 1:
@@ -24,5 +32,5 @@ class BirthdayMessageController(BaseController):
 
             message = cls.fill_from_template(f'<@{employee_id}>', template)
             best_matching_keyword = cls.get_best_matching_template_keyword(message)
-            selected_gif = gif_integration.get_random_gif(best_matching_keyword, cls.gif_search_limit)
-            slack_message_integration.send_message(message, selected_gif.get('url'), selected_gif.get('description'))
+            message_coros.append(send_message_coro(best_matching_keyword, cls.gif_search_limit, message))
+        await asyncio.gather(*message_coros)
