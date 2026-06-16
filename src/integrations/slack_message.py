b9c004d15c5cd73cea777c81a985c5e1442c5593
Ivan <ivan@ivancorniel-ec1864.local>
2026-06-16 11:09:29 -0400

    Replace-Tenor-with-giphy

--- src/integrations/slack_message.py
+++ src/integrations/slack_message.py
@@ -1,14 +1,27 @@
-from src.clients import RestClient
+import asyncio
+from functools import partial
+
+import requests
+
 from src.config import EnvManager
 
 
 class SlackMessageIntegration:
     webhook_url: str = EnvManager.SLACK_WEBHOOK_URL_SECRET
-    client = RestClient('https://hooks.slack.com/services/')
+
+    @classmethod
+    async def _post(cls, payload: dict) -> requests.Response:
+        partial_post = partial(requests.post, cls.webhook_url, json=payload)
+        response = await asyncio.get_event_loop().run_in_executor(None, partial_post)
+        if response.status_code != 200 or response.text != 'ok':
+            raise Exception(
+                f'Could not send Slack message. Status: {response.status_code}, response: {response.text}'
+            )
+        return response
 
     @classmethod
     async def send_message(cls, text_message: str, image_url: str, image_alt_text: str = ''):
-        response = await cls.client.post(cls.webhook_url, payload={
+        return await cls._post({
             'text': text_message,
             'blocks': [
                 {
@@ -25,8 +38,7 @@ class SlackMessageIntegration:
                 }
             ]
         })
-        return response
 
     @classmethod
     async def send_raw_message(cls, raw_message: dict):
-        return await cls.client.post(cls.webhook_url, payload=raw_message)
+        return await cls._post(raw_message)
