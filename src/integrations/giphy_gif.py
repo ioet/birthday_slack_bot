b7c0aa7922f9b5608c49b4b3ce316c215d631926
Ivan <ivan@ivancorniel-ec1864.local>
2026-06-19 15:45:43 -0400

    make gifs at least 200 px and at most 400 px wide; fix iam permissions for deployment; test script for local developent sending message to slack

--- src/integrations/giphy_gif.py
+++ src/integrations/giphy_gif.py
@@ -8,6 +8,39 @@ class GiphyGifIntegration:
     client = RestClient('https://api.giphy.com/v1/gifs')
     api_key: str = EnvManager.GIPHY_API_KEY
     failback_gif: str = ''
+    min_width: int = 200
+    max_width: int = 400
+    fallback_renditions: tuple = ('fixed_width', 'fixed_height', 'downsized_medium', 'downsized')
+
+    @classmethod
+    def _select_image_url(cls, images: dict) -> str:
+        if not images:
+            return cls.failback_gif
+
+        in_range = []
+        for image in images.values():
+            url = image.get('url')
+            width = image.get('width')
+            if not url or width is None:
+                continue
+            try:
+                width = int(width)
+            except (TypeError, ValueError):
+                continue
+            if cls.min_width <= width <= cls.max_width:
+                in_range.append((width, url))
+
+        if in_range:
+            target_width = (cls.min_width + cls.max_width) // 2
+            in_range.sort(key=lambda item: abs(item[0] - target_width))
+            return in_range[0][1]
+
+        for rendition in cls.fallback_renditions:
+            url = images.get(rendition, {}).get('url')
+            if url:
+                return url
+
+        return images.get('fixed_height_small', {}).get('url') or cls.failback_gif
 
     @classmethod
     async def get_random_gif(cls, search_criteria: str, limit: int = 2) -> dict:
@@ -19,8 +52,7 @@ class GiphyGifIntegration:
         options = response.json().get('data', [])
         selected_gif = choice(options)
         images = selected_gif.get('images', {})
-        url = cls.failback_gif if not images else images.get('fixed_height_small', {}).get('url')
         return {
             'description': selected_gif.get('title', ''),
-            'url': url,
+            'url': cls._select_image_url(images),
         }
